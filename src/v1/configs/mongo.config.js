77adc14d980b845a193b47ab143a491e57c86603
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-01-31 21:46:27 +0600

    Move mongoDb connection options to mongo.config file

--- src/v1/configs/mongo.config.js
+++ src/v1/configs/mongo.config.js
@@ -1,8 +1,17 @@
 require('dotenv').config();
+
 const config = {
   url: process.env.MONGO_URL,
   username: process.env.MONGO_USERNAME,
   password: process.env.MONGO_PASSWORD,
 };
 
-module.exports = config;
+const options = {
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4,
+  useNewUrlParser: true,
+};
+
+module.exports = { config, options };
