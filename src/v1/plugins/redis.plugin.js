3d391c89d5ffb8e3cd193bd73b695fee8315568e
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 04:02:16 +0600

    Update redis callback function name

--- src/v1/plugins/redis.plugin.js
+++ src/v1/plugins/redis.plugin.js
@@ -6,21 +6,21 @@ const redisClient = createClient({
   url: redisConfig.url,
 });
 
-connectCallback = () => {
+redisConnectCallback = () => {
   console.log(
     'redis-connect-callback-response',
     'Connection to redis successful',
   );
 };
 
-errorCallback = (error) => {
+redisErrorCallback = (error) => {
   console.log('redis-error-callback-error', error);
   redisClient.disconnect();
   process.exit();
 };
 
 redisClient.connect();
-redisClient.on('connect', connectCallback);
-redisClient.on('error', errorCallback);
+redisClient.on('connect', redisConnectCallback);
+redisClient.on('error', redisErrorCallback);
 
 module.exports = redisClient;
