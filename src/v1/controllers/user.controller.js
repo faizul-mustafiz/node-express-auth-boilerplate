2b25ae3541bf31c440aceebc64ef7e441f62bb77
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 04:00:43 +0600

    Add user controller placeholder methods

---
+++ src/v1/controllers/user.controller.js
@@ -0,0 +1,11 @@
+getAllUser = async (req, res, next) => {};
+getOneUser = async (req, res, next) => {};
+updateOneUser = async (req, res, next) => {};
+deleteOneUser = async (req, res, next) => {};
+
+module.exports = {
+  getAllUser,
+  getOneUser,
+  updateOneUser,
+  deleteOneUser,
+};
