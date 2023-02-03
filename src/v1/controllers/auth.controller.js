fc5fbecee6ad9c72913d1d93ed18d566a26d6679
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 04:01:07 +0600

    Add auth controller placeholder methods

---
+++ src/v1/controllers/auth.controller.js
@@ -0,0 +1,19 @@
+signUp = async (req, res, next) => {};
+signIn = async (req, res, next) => {};
+signOut = async (req, res, next) => {};
+verify = async (req, res, next) => {};
+resetPassword = async (req, res, next) => {};
+changePassword = async (req, res, next) => {};
+revokeAccessToken = async (req, res, next) => {};
+revokeRefreshToken = async (req, res, next) => {};
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  verify,
+  resetPassword,
+  changePassword,
+  revokeAccessToken,
+  revokeRefreshToken,
+};
