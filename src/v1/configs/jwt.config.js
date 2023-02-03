74ccdffd9acb4f96ed255553fcfab723eb89542c
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 03:59:59 +0600

    Add jwt configuration on config file

--- src/v1/configs/jwt.config.js
+++ src/v1/configs/jwt.config.js
@@ -1,24 +1,50 @@
 require('dotenv').config();
 const {
   ACCESS_TOKEN_SECRET,
-  REFRESH_TOKEN_SECRET,
-  VERIFY_TOKEN_SECRET,
-  RESET_PASSWORD_TOKEN_SECRET,
-  CHANGE_PASSWORD_TOKEN_SECRET,
   ACCESS_TOKEN_EXPIRY_TIME,
+  REFRESH_TOKEN_SECRET,
   REFRESH_TOKEN_EXPIRY_TIME,
+  VERIFY_TOKEN_SECRET,
+  VERIFY_TOKEN_EXPIRY_TIME,
+  RESET_PASSWORD_TOKEN_SECRET,
+  RESET_PASSWORD_TOKEN_EXPIRY_TIME,
+  CHANGE_PASSWORD_TOKEN_SECRET,
+  CHANGE_PASSWORD_TOKEN_EXPIRY_TIME,
   PUBLIC_KEY,
   PRIVATE_KEY,
 } = require('../environments');
 
-module.exports = {
-  accessTokenSecret: ACCESS_TOKEN_SECRET,
-  refreshTokenSecret: REFRESH_TOKEN_SECRET,
+const accessToken = {
+  secret: ACCESS_TOKEN_SECRET,
+  expiryTime: ACCESS_TOKEN_EXPIRY_TIME,
+};
+
+const refreshToken = {
+  secret: REFRESH_TOKEN_SECRET,
+  expiryTime: REFRESH_TOKEN_EXPIRY_TIME,
+};
+
+const verifyToken = {
   verifyTokenSecret: VERIFY_TOKEN_SECRET,
-  resetPasswordTokenSecret: RESET_PASSWORD_TOKEN_SECRET,
-  changePasswordTokenSecret: CHANGE_PASSWORD_TOKEN_SECRET,
-  accessTokenExpiryTime: ACCESS_TOKEN_EXPIRY_TIME,
-  refreshTokenExpiryTime: REFRESH_TOKEN_EXPIRY_TIME,
+  expiryTime: VERIFY_TOKEN_EXPIRY_TIME,
+};
+
+const resetPasswordToken = {
+  secret: RESET_PASSWORD_TOKEN_SECRET,
+  expiryTime: RESET_PASSWORD_TOKEN_EXPIRY_TIME,
+};
+
+const changePasswordToken = {
+  secret: CHANGE_PASSWORD_TOKEN_SECRET,
+  expiryTime: CHANGE_PASSWORD_TOKEN_EXPIRY_TIME,
+};
+
+module.exports = {
+  accessToken,
+  refreshToken,
+  verifyToken,
+  resetPasswordToken,
+  changePasswordToken,
   publicKey: PUBLIC_KEY,
   privateKey: PRIVATE_KEY,
 };
