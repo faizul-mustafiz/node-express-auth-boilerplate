edb87591f977b8c535ab3a591d3848f0dee6d4de
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-03 22:52:47 +0600

    Add jwt related environment variables

--- src/v1/environments/environment.production.js
+++ src/v1/environments/environment.production.js
@@ -10,6 +10,16 @@ module.exports = {
   MONGO_URL: process.env.MONGO_URL,
   MONGO_USERNAME: process.env.MONGO_USERNAME,
   MONGO_PASSWORD: process.env.MONGO_PASSWORD,
-  // hashing algorithm fro encryption
+  // hashing algorithm for encryption
   HASHING_ALGORITHM: process.env.HASHING_ALGORITHM,
+  // JWT environment variables
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET,
+  VERIFY_TOKEN_SECRET: process.env.VERIFY_TOKEN_SECRET,
+  RESET_PASSWORD_TOKEN_SECRET: process.env.RESET_PASSWORD_TOKEN_SECRET,
+  CHANGE_PASSWORD_TOKEN_SECRET: process.env.CHANGE_PASSWORD_TOKEN_SECRET,
+  ACCESS_TOKEN_EXPIRY_TIME: process.env.ACCESS_TOKEN_EXPIRY_TIME,
+  REFRESH_TOKEN_EXPIRY_TIME: process.env.REFRESH_TOKEN_EXPIRY_TIME,
+  PUBLIC_KEY: process.env.PUBLIC_KEY,
+  PRIVATE_KEY: process.env.PRIVATE_KEY,
 };
