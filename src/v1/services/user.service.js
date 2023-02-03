3e9e989151e279e18fbd7c74202dbea80d7dbff7
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 03:57:21 +0600

    Add user service placeholder methods

---
+++ src/v1/services/user.service.js
@@ -0,0 +1,11 @@
+getAll = async () => {};
+getOne = async (id) => {};
+updateOne = async (id, payload) => {};
+deleteOne = async (id) => {};
+
+module.exports = {
+  getAll,
+  getOne,
+  updateOne,
+  deleteOne,
+};
