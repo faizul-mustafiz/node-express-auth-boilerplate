6e39f71a99040d53502abd8beb522d5f2095a581
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 05:08:26 +0600

    Remove baseApiRout config form app.js and add add static routes

--- src/v1/app.js
+++ src/v1/app.js
@@ -3,7 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { AuthRoutes, UserRoutes } = require('./routes');
-const { baseRoute } = require('./configs/app.config');
 const app = express();
 app.use(express.json());
 app.use(
@@ -14,11 +13,11 @@ app.use(
 app.use(bodyParser.json({ limit: '20mb' }));
 app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }));
 app.use(cors());
-app.get(`${baseRoute}/health`, (req, res) => {
+app.get('/api/v1/health', (req, res) => {
   res.status(200).json({ message: 'Basic Health Check.' });
 });
-app.use(`${baseRoute}/auth`, AuthRoutes);
-app.use(`${baseRoute}/users`, UserRoutes);
+app.use('/api/v1/auth', AuthRoutes);
+app.use('/api/v1/users', UserRoutes);
 
 //redis connection
 const redisPlugin = require('./plugins/redis.plugin');
