2614d3834deaea68da86a3dcc459571c460cdbff
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-04 21:44:21 +0600

    Add basic sign-up mechanism without tokens.

--- src/v1/controllers/auth.controller.js
+++ src/v1/controllers/auth.controller.js
@@ -1,4 +1,33 @@
-signUp = async (req, res, next) => {};
+require('dotenv');
+const User = require('../models/user.model');
+
+signUp = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.emailExist(email);
+    console.log('existingUser', existingUser);
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: 'An account with this email already exists',
+        result: {},
+      });
+    }
+    const user = new User({ email: email, password: password });
+    user.password = await User.generateHash(password);
+    console.log('user', user);
+    const result = await user.save();
+    res.status(201).json({
+      success: true,
+      message: 'Successfully use created',
+      result,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Oops there is an Error', result: {} });
+  }
+};
 signIn = async (req, res, next) => {};
 signOut = async (req, res, next) => {};
 verify = async (req, res, next) => {};
