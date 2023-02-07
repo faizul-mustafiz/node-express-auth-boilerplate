ae2142f404d4261f522915f5c270d2f098471416
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-08 01:47:22 +0600

    Merge two separate  method for token payload to one single payload.

--- src/v1/utility/jwt.utility.js
+++ src/v1/utility/jwt.utility.js
@@ -1,33 +1,25 @@
 const _crypto = require('crypto');
 const uuid = require('uuid').v4();
 
-// The data must be of type string or an instance of Buffer, TypedArray, or DataView.
-// If you want to pass json first stringify and then pass the data.
+/**
+ * * The data must be of type string or an instance of Buffer, TypedArray, or DataView.
+ * *If you want to pass json first stringify and then pass the data.
+ */
 generateIdentityHash = (data) => {
-  return _crypto.createHash('md5').update(data).digest('hex');
+  return _crypto.createHash('sha1').update(data).digest('hex');
 };
 generateTokenId = () => {
   return uuid;
 };
-
-generateAccessTokenData = (user) => {
+generateTokenPayloadForRedis = (user, type) => {
   return {
-    user_id: user._id,
+    user_id: user.id,
     user_email: user.email,
-    type: 'access',
+    type: type,
   };
 };
-generateRefreshTokenData = (user) => {
-  return {
-    user_id: user._id,
-    user_email: user.email,
-    type: 'refresh',
-  };
-};
-
 module.exports = {
   generateIdentityHash,
   generateTokenId,
-  generateAccessTokenData,
-  generateRefreshTokenData,
+  generateTokenPayloadForRedis,
 };
