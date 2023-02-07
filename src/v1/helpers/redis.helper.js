e8379bc6b8de1e3533790b1e7bb7670eabf7f99f
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-08 01:49:16 +0600

    Update redis key value set mechanism. Token identity is now set by hashSet. and also refactor token blacklist check and set mechanism. Also update key delete methods

--- src/v1/helpers/redis.helper.js
+++ src/v1/helpers/redis.helper.js
@@ -1,37 +1,56 @@
 const redisClient = require('../plugins/redis.plugin');
 const { base64ToJson } = require('../helpers/conversion.helper');
 
-setTokenIdentity = async (identity, expiry, payload) => {
+saveTokenIdentityWithPayload = async (identity, expiry, payload) => {
+  console.log('identity', identity);
+  console.log('expiry', expiry);
+  console.log('payload', payload);
   try {
-    const result = await redisClient.setEx(identity, expiry, payload);
+    let result = await redisClient.hSet(identity, [
+      'user_id',
+      payload.user_id,
+      'user_email',
+      payload.user_email,
+    ]);
+    result = await redisClient.expireAt(identity, expiry);
     console.log('setTokenIdentity-result', result);
   } catch (error) {
     console.log('setTokenIdentity-error', error);
   }
 };
 
-setTokenId = async (tokenId, expiry) => {
+addTokenToBlacklist = async (identity, expiry) => {
   try {
-    const result = await redisClient.setEx(tokenId, expiry, '');
+    const result = await redisClient.setEx(`bl_${identity}`, expiry, '');
     console.log('setTokenId-result', result);
   } catch (error) {
     console.log('setTokenId-error', error);
   }
 };
 
-getTokenByIdentity = async (identity) => {
+getTokenPayload = async (identity) => {
   try {
-    let result = await redisClient.get(identity);
-    console.log('getTokenByIdentity-result', result);
-    return base64ToJson(result);
+    let result = await redisClient.hGetAll(identity);
+    console.log('getTokenPayload-result', result);
+    return result;
   } catch (error) {
-    console.log('getTokenByIdentity-error', error);
+    console.log('getTokenPayload-error', error);
   }
 };
 
-isTokenBlacklisted = async (tokenId) => {
+isTokenIdentityExists = async (identity) => {
   try {
-    let result = await redisClient.get(identity);
+    let result = await redisClient.exists(identity);
+    console.log('isTokenIdentityExists-result', result);
+    return result;
+  } catch (error) {
+    console.log('isTokenIdentityExists-error', error);
+  }
+};
+
+isTokenBlacklisted = async (identity) => {
+  try {
+    let result = await redisClient.exists(`bl_${identity}`);
     console.log('isTokenBlacklisted-result', result);
     return result;
   } catch (error) {
@@ -39,19 +58,30 @@ isTokenBlacklisted = async (tokenId) => {
   }
 };
 
-deleteKey = async (key) => {
+deleteTokenPayload = async (identity) => {
   try {
-    let result = await redisClient.del(key);
-    console.log('deleteKey-result', result);
+    let result = await redisClient.del(identity);
+    console.log('deleteTokenPayload-result', result);
   } catch (error) {
-    console.log('deleteKey-error', error);
+    console.log('deleteTokenPayload-error', error);
+  }
+};
+
+deleteBlacklistedToken = async (identity) => {
+  try {
+    let result = await redisClient.del(`bl_${identity}`);
+    console.log('deleteBlacklistedToken-result', result);
+  } catch (error) {
+    console.log('deleteBlacklistedToken-error', error);
   }
 };
 
 module.exports = {
-  setTokenIdentity,
-  setTokenId,
-  getTokenByIdentity,
+  saveTokenIdentityWithPayload,
+  addTokenToBlacklist,
+  getTokenPayload,
+  isTokenIdentityExists,
   isTokenBlacklisted,
-  deleteKey,
+  deleteTokenPayload,
+  deleteBlacklistedToken,
 };
