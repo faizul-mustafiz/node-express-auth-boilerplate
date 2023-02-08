f812acfc7667935ea7612b900bc20a40711c5d02
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-08 23:49:13 +0600

    Update sign-up method to serve verify token and otp.

--- src/v1/controllers/auth.controller.js
+++ src/v1/controllers/auth.controller.js
@@ -1,14 +1,86 @@
 require('dotenv');
 const User = require('../models/schema/user.model');
-const { signAccessToken, signRefreshToken } = require('../helpers/jwt.helper');
+const {
+  signAccessToken,
+  signRefreshToken,
+  signVerifyToken,
+} = require('../helpers/jwt.helper');
 const {
   generateIdentityHash,
   generateTokenPayloadForRedis,
+  generateVerifyTokenPayloadForRedis,
+  generateOtp,
+  generatePasswordHash,
 } = require('../utility/jwt.utility');
 
 const TokenType = require('../models/static/token-type.model');
-
 signUp = async (req, res, next) => {
+  try {
+    let { email, password } = req.body;
+    // console.log('signUp-req', req.body);
+    password = generatePasswordHash(password);
+    console.log(email, password);
+
+    const existingUser = await User.emailExist(email);
+    console.log('existingUser', existingUser);
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: 'An account with this email already exists',
+        result: {},
+      });
+    }
+
+    const OTP = generateOtp(8);
+    console.log('OTP', OTP);
+
+    /**
+     * * generate verify token payload data that needs to be stored in redis
+     */
+    const verifyTokenPayload = generateVerifyTokenPayloadForRedis(
+      email,
+      password,
+      TokenType.Verify,
+      OTP,
+    );
+    console.log('verifyTokenPayload', verifyTokenPayload);
+    /**
+     * * generate verify token identity hash for redis key
+     */
+    const verifyTokenIdentity = generateIdentityHash(
+      JSON.stringify(verifyTokenPayload),
+    );
+    /**
+     * * generate verify token.
+     */
+    const verifyToken = await signVerifyToken(
+      verifyTokenIdentity,
+      verifyTokenPayload,
+    );
+
+    /**
+     * * generate verify token response
+     */
+    const result = {
+      verify_token: verifyToken,
+      otp: OTP,
+    };
+
+    res.status(200).json({
+      success: true,
+      message: 'Please verify OTP for successful sign-up',
+      result,
+    });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ success: false, message: 'Oops there is an Error', result: {} });
+  }
+};
+signIn = async (req, res, next) => {};
+signOut = async (req, res, next) => {};
+verify = async (req, res, next) => {
   try {
     const { email, password } = req.body;
     const existingUser = await User.emailExist(email);
@@ -89,9 +161,6 @@ signUp = async (req, res, next) => {
       .json({ success: false, message: 'Oops there is an Error', result: {} });
   }
 };
-signIn = async (req, res, next) => {};
-signOut = async (req, res, next) => {};
-verify = async (req, res, next) => {};
 resetPassword = async (req, res, next) => {};
 changePassword = async (req, res, next) => {};
 refresh = async (req, res, next) => {};
