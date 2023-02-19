9a4c7f84a28afd3b98986140613b835820af47b0
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-19 21:57:20 +0600

    Add hasAuthorization and validateAccess middleware for all  users route

--- src/v1/routes/user.routes.js
+++ src/v1/routes/user.routes.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const userRouter = express.Router();
 const { UserController } = require('../controllers/index');
+const hasAuthorization = require('../middlewares/hasAuthorization.middleware');
+const validateAccess = require('../middlewares/validateAccess.middleware');
 
-userRouter.get('/', UserController.getAllUser);
-userRouter.get('/:userId', UserController.getOneUser);
-userRouter.post('/:userId', UserController.updateOneUser);
-userRouter.delete('/:userId', UserController.deleteOneUser);
+userRouter.get(
+  '/',
+  [hasAuthorization, validateAccess],
+  UserController.getAllUser,
+);
+userRouter.get(
+  '/:userId',
+  [hasAuthorization, validateAccess],
+  UserController.getOneUser,
+);
+userRouter.post(
+  '/:userId',
+  [hasAuthorization, validateAccess],
+  UserController.updateOneUser,
+);
+userRouter.delete(
+  '/:userId',
+  [hasAuthorization, validateAccess],
+  UserController.deleteOneUser,
+);
 module.exports = userRouter;
