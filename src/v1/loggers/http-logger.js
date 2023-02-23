0f9224aa37d83fc6d97435606b04ac4432a4c964
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-02-23 22:54:08 +0600

    Add httpLogger middleware This middleware will log every request ever made to the api with method, url, timestamp, status, contentLength, and responseTime.

---
+++ src/v1/loggers/http-logger.js
@@ -0,0 +1,36 @@
+/**
+ * * HTTP request logger. this logger will log every request ever made to your api
+ * * with the time of the request, method, status, url, responseContentLength and responseTime
+ * * this middleware is injected at the main app.js
+ * * app.use(httpLogger)
+ */
+const morgan = require('morgan');
+const json = require('morgan-json');
+const format = json({
+  method: ':method',
+  url: ':url',
+  status: ':status',
+  resContentLength: ':res[content-length]',
+  responseTime: ':response-time',
+});
+const logger = require('./logger');
+const httpLogger = morgan(format, {
+  stream: {
+    write: (message) => {
+      console.log('message', message);
+      console.log('json-parse-message', JSON.parse(message));
+      const { method, url, status, resContentLength, responseTime } =
+        JSON.parse(message);
+      logger.info('HTTP-Request-Log', {
+        timestamp: new Date(),
+        method,
+        url,
+        status: Number(status),
+        responseTime: Number(responseTime),
+        resContentLength,
+      });
+    },
+  },
+});
+
+module.exports = httpLogger;
