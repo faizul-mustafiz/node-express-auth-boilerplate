45078dd0da988a31dc5c755062b3fa779cf29e5a
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-03-28 00:59:42 +0600

    Import app protocol info in app.config

--- src/v1/configs/app.config.js
+++ src/v1/configs/app.config.js
@@ -1,7 +1,13 @@
-const { BASE_API_ROUTE, API_PORT, API_HOST } = require('../environments');
+const {
+  BASE_API_ROUTE,
+  API_PORT,
+  API_HOST,
+  API_PROTOCOL,
+} = require('../environments');
 
 module.exports = {
   baseRoute: BASE_API_ROUTE,
   port: API_PORT,
   host: API_HOST,
+  protocol: API_PROTOCOL,
 };
