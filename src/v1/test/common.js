b06b8a0a41204bc7e61cdf3f1b6d0355697e0e48
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-03-30 21:54:58 +0600

    Add common.js for all test related data

---
+++ src/v1/test/common.js
@@ -0,0 +1,20 @@
+const testUserObj = {
+  email: 'test@gmail.com',
+  password: '123456',
+};
+const testNewPassword = '12345678';
+const testUserUpdateObj = {
+  email: 'johndoe@gmail.com',
+  password: '12345678',
+  name: 'John Doe',
+  avatar: 'https://pixabay.com/images/id-973460/',
+  mobile: '+8801700000000',
+  dob: '1990-09-09',
+  organization: 'Evil Corp',
+};
+
+module.exports = {
+  testUserObj,
+  testNewPassword,
+  testUserUpdateObj,
+};
