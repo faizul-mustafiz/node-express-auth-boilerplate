4d1ef0920dca99fe19bd268c4e37771b8e222b2a
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-03-30 21:55:32 +0600

    Update auth.test for new common.js and also added comments

--- src/v1/test/auth.test.js
+++ src/v1/test/auth.test.js
@@ -1,32 +1,32 @@
 /**
- * * During the test we are going to use test environment config
- * * as mongo will use auth_test db and redis will use db index 15
- * @example redis://redis:6379/15
- * * these dbs are assigned for our test environment
+ * * import baseRoute, User model and test static data
  */
-process.env.NODE_ENV = 'test';
-
 const { baseRoute } = require('../configs/app.config');
 const User = require('../models/user.model');
-
+const { testUserObj, testNewPassword } = require('./common');
+/**
+ * * import chai, chai-http dependencies
+ * * also inject server for mocha to run tests
+ * * import should aggregator from chai
+ * * use chai-http with chai to perform http request
+ */
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../../../index');
 const should = chai.should();
-
 chai.use(chaiHttp);
-
-const newUser = {
-  email: 'test@gmail.com',
-  password: '123456',
-};
+/**
+ * * global variables needed for the entire test file
+ */
 let verifyToken = '';
 let verifyCode = '';
 let forgotPassToken = '';
 let forgotPassCode = '';
 let accessToken = '';
 let refreshToken = '';
-
+/**
+ * * all global variable reset method
+ */
 resetAllTestVariables = () => {
   verifyToken = '';
   verifyCode = '';
@@ -35,25 +35,38 @@ resetAllTestVariables = () => {
   accessToken = '';
   refreshToken = '';
 };
-
+/**
+ * * auth controller endpoint test cases
+ */
 describe('Auth controller tests', () => {
+  /**
+   * @before will run at the start of the test cases
+   * * here we delete all the old data from test db user collection
+   */
   before((done) => {
     User.deleteMany({}, () => {
       done();
     });
   });
-
+  /**
+   * @after will run after the last test cases of the file
+   * * here we reset all the global variables user for the entire test file
+   */
   after((done) => {
     resetAllTestVariables();
     done();
   });
-
+  /**
+   * * complete sign-up process test. Here this test will first request the /sign-up
+   * * route and the get the verify token and code and using this token and code will
+   * * compete sign-up using /verify route
+   */
   describe('[POST] /auth/sign-up |Sign-Up Process Test', () => {
     it('it should initiate sign-up process using route: [POST] /auth/sign-up', (done) => {
       chai
         .request(server)
         .post(`${baseRoute}/auth/sign-up`)
-        .send(newUser)
+        .send(testUserObj)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -82,7 +95,7 @@ describe('Auth controller tests', () => {
           res.body.should.have.property('message');
           res.body.should.have.property('result');
           res.body.result.should.be.a('object');
-          res.body.result.should.have.property('email').eql(newUser.email);
+          res.body.result.should.have.property('email').eql(testUserObj.email);
           res.body.result.should.have.property('_id');
           res.body.result.should.have.property('accessToken');
           res.body.result.should.have.property('refreshToken');
@@ -92,13 +105,17 @@ describe('Auth controller tests', () => {
         });
     });
   });
-
+  /**
+   * * complete sign-in process test. Here this test will first request the /sign-in
+   * * route and the get the verify token and code and using this token and code will
+   * * compete sign-in using /verify route
+   */
   describe('[POST] /auth/sign-in |Sign-in Process Test', () => {
     it('it should initiate sign-in process using route: [POST] /auth/sign-in ', (done) => {
       chai
         .request(server)
         .post(`${baseRoute}/auth/sign-in`)
-        .send(newUser)
+        .send(testUserObj)
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -135,13 +152,15 @@ describe('Auth controller tests', () => {
         });
     });
   });
-
+  /**
+   * * forgot-password process test.
+   */
   describe('[POST] /auth/forgot-password |Forgot-Password Process Test', () => {
     it('it should initiate forgot-password process', (done) => {
       chai
         .request(server)
         .post(`${baseRoute}/auth/forgot-password`)
-        .send({ email: newUser.email })
+        .send({ email: testUserObj.email })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -157,14 +176,17 @@ describe('Auth controller tests', () => {
         });
     });
   });
-
+  /**
+   * * change-password process test. here this test will use the forgotPasswordToken and
+   * * forgotPasswordCode to perform change-password process
+   */
   describe('[POST] /auth/change-password |Change-Password Process Test', () => {
     it('it should complete change-password process', (done) => {
       chai
         .request(server)
         .post(`${baseRoute}/auth/change-password`)
         .set('Authorization', `Bearer ${forgotPassToken}`)
-        .send({ code: forgotPassCode, new_password: '12346578' })
+        .send({ code: forgotPassCode, new_password: testNewPassword })
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('object');
@@ -176,7 +198,10 @@ describe('Auth controller tests', () => {
         });
     });
   });
-
+  /**
+   * * refresh process test. here this test will use the refresh token to
+   * * get new pair of accessToken and refreshToken
+   */
   describe('[POST] /auth/refresh |Refresh [tokens] Process Test', () => {
     it('it should complete refresh both access and refresh token process', (done) => {
       chai
@@ -199,7 +224,10 @@ describe('Auth controller tests', () => {
         });
     });
   });
-
+  /**
+   * * revoke-access-token process test. here this test will use the access token
+   * * to revoke the access token
+   */
   describe('[POST] /auth/revoke-at |Revoke-Access-Token Process Test', () => {
     it('it should complete access token revoke process', (done) => {
       chai
@@ -218,7 +246,10 @@ describe('Auth controller tests', () => {
         });
     });
   });
-
+  /**
+   * * revoke-refresh-token process test. here this test will use the refresh token
+   * * to revoke the access token
+   */
   describe('[POST] /auth/revoke-rt |Revoke-Refresh-Token Process Test', () => {
     it('it should complete access token revoke process', (done) => {
       chai
