a69c6e519ee1274ec333985c355d06f0d36f8fef
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-03-30 21:56:18 +0600

    Inject the test NODE_ENV inside the index.js as this is inherited to all the test files

--- src/v1/test/index.test.js
+++ src/v1/test/index.test.js
@@ -1,3 +1,10 @@
+/**
+ * * During the test we are going to use test environment config
+ * * as mongo will use auth_test db and redis will use db index 15
+ * @example redis://redis:6379/15
+ * * these dbs are assigned for our test environment
+ */
+process.env.NODE_ENV = 'test';
 const authTest = require('./auth.test');
 const userTest = require('./user.test');
 module.exports = {
